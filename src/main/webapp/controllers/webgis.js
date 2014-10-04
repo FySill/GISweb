8bf8b03ea7dfc164e11c68b3dd275beb66e299d0
chengfei <fysill@hotmail.com>
2014-10-04 18:46:11 +0800

    table2

--- src/main/webapp/controllers/webgis.js
+++ src/main/webapp/controllers/webgis.js
@@ -1,7 +1,80 @@
 //function webGis($scope) {
 //	$scope.loadingData = function () {
-	    require(["dojo/on", "dojo/query", "dojo/parser", "dojo/ready", "dijit/layout/BorderContainer", "dijit/layout/ContentPane", "dojo/dom", "esri/map", "esri/geometry/Extent", "esri/urlUtils", "esri/arcgis/utils", "esri/dijit/Legend", "esri/dijit/Scalebar", "dojo/domReady!"], 
-        function( on, query, parser, ready, BorderContainer, ContentPane, dom, Map, Extent, urlUtils, arcgisUtils, Legend, Scalebar) {
+
+	    require(["dojo/on", "dojo/query", "dojo/parser", "esri/layers/FeatureLayer", "esri/tasks/query",
+	             "dojo/ready", "dijit/layout/BorderContainer", 
+	             "dijit/layout/ContentPane", "dojo/dom", "esri/map", 
+	             "esri/geometry/Extent", "esri/urlUtils", "esri/arcgis/utils", 
+	             "esri/dijit/Legend", "esri/dijit/Scalebar",
+	             "dgrid/OnDemandGrid", "dojo/store/Memory", "dojo/domReady!"], 
+        function( on, query, parser, FeatureLayer, Query, ready, BorderContainer, ContentPane, 
+        		dom, Map, Extent, urlUtils, arcgisUtils, Legend, Scalebar,
+        		OnDemandGrid, Memory) {
+	    	var featureLayer, pageInfo, grid;
+	    	// create a dgrid 
+	        var sortAttr = [{
+	          attribute: "OBJECTID",
+	          descending: true
+	        }];
+	        grid = new OnDemandGrid({
+	          store: Memory({
+	            idProperty: "OBJECTID"
+	          }),
+	          columns:{
+	        	OBJECTID: "OBJECTID",
+	            JCDBH: "JCDBH",
+	            JZDJ: "JZDJ",
+	            SCZJ: "SCZJ",
+	          },
+	          sort: sortAttr
+	        }, "grid");
+	     // create a feature layer
+	        featureLayer = new FeatureLayer("http://192.168.2.127:6080/arcgis/rest/services/webgis/cx/MapServer/2", {
+	          outFields:["*"]
+	        });
+	     // get object IDs from the table (feature layer)
+	        featureLayer.on("load", function () {
+	          // create the query to fetch object IDs for earthquakes that have a magnitude greater than 6.0
+	          // that occurred after January 1st 2007
+	          var query = new Query();
+	          query.where = "OBJECTID > 1";
+//	          query.timeExtent = new TimeExtent(new Date("01/01/2007 UTC"));
+	          featureLayer.queryIds(query, function (objectIds) {
+	        	  console.log("objectids:" + objectIds);
+	            fetchRecords(objectIds);
+	          });
+	          // Query for the records with the given object IDs and populate the grid
+//	            featureLayer.queryFeatures(query, function (featureSet) {
+//	            	console.log("featureSet:" + featureSet);
+//	              updateGrid(featureSet, pageNumber);
+//	            });
+	        });
+	        function fetchRecords(objectIds) {
+	            if (objectIds.length > 0) {
+	            	var query = new Query();
+	                query.objectIds = objectIds;
+	                query.outFields = ["*"];
+	                featureLayer.queryFeatures(query, function (featureSet) {
+	                	console.log("featureSet:" + featureSet);
+//	              		updateGrid(featureSet, pageNumber);
+	                });
+//	              updatePageInformation(objectIds);
+//	              queryRecordsByPage(1);
+	            } else {
+	              grid.showMessage("No matching records");
+	              grid.setStore(null);
+	            }
+	        };
+	        function updatePageInformation(objectIds, page) {
+	            pageInfo = {
+	              objectIds: objectIds,
+	              totalRecords: objectIds.length,
+	              totalPages: Math.ceil(objectIds.length / 15),
+	              currentPage: page || 0,
+	              recordsPerPage: 15
+	            };
+	        };
+	        
 	    	// Wire UI Events
 	    	var layerIsHide = true;
 	    	var frist = true;
@@ -21,7 +94,7 @@
 	        		for (var i = 1; i < length; i++){
 	        		on($('#legend_operational0')[0].childNodes[i],"click", function(e) { 
 		            	if (isHide ) {
-		            		$("#webtable").animate({top:150,opacity:'show',width:150,height:400,left:'40%'},500);
+		            		$("#webtable").animate({top:150,opacity:'show',width:500,height:300,left:'30%'},500);
 		            		text = e.currentTarget.innerText;
 		            		isHide = false;
 		            	}else if(text != e.currentTarget.innerText){
