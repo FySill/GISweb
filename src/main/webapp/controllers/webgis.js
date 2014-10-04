ae3905c8b2cd72e0005ecf2ff17c83da32ce4f9f
chengfei <fysill@hotmail.com>
2014-10-05 01:24:31 +0800

    tableCom

--- src/main/webapp/controllers/webgis.js
+++ src/main/webapp/controllers/webgis.js
@@ -1,14 +1,14 @@
 //function webGis($scope) {
 //	$scope.loadingData = function () {
-
+		var layerNum = 1;
 	    require(["dojo/on", "dojo/query", "dojo/parser", "esri/layers/FeatureLayer", "esri/tasks/query",
 	             "dojo/ready", "dijit/layout/BorderContainer", 
 	             "dijit/layout/ContentPane", "dojo/dom", "esri/map", 
 	             "esri/geometry/Extent", "esri/urlUtils", "esri/arcgis/utils", 
-	             "esri/dijit/Legend", "esri/dijit/Scalebar",
+	             "esri/dijit/Legend", "esri/dijit/Scalebar","dojo/_base/array",
 	             "dgrid/OnDemandGrid", "dojo/store/Memory", "dojo/domReady!"], 
         function( on, query, parser, FeatureLayer, Query, ready, BorderContainer, ContentPane, 
-        		dom, Map, Extent, urlUtils, arcgisUtils, Legend, Scalebar,
+        		dom, Map, Extent, urlUtils, arcgisUtils, Legend, Scalebar,arrayUtils,
         		OnDemandGrid, Memory) {
 	    	var featureLayer, pageInfo, grid;
 	    	// create a dgrid 
@@ -16,54 +16,42 @@
 	          attribute: "OBJECTID",
 	          descending: true
 	        }];
-	        grid = new OnDemandGrid({
-	          store: Memory({
-	            idProperty: "OBJECTID"
-	          }),
-	          columns:{
-	        	OBJECTID: "OBJECTID",
-	            JCDBH: "JCDBH",
-	            JZDJ: "JZDJ",
-	            SCZJ: "SCZJ",
-	          },
-	          sort: sortAttr
-	        }, "grid");
-	     // create a feature layer
-	        featureLayer = new FeatureLayer("http://192.168.2.127:6080/arcgis/rest/services/webgis/cx/MapServer/2", {
-	          outFields:["*"]
-	        });
-	     // get object IDs from the table (feature layer)
-	        featureLayer.on("load", function () {
-	          // create the query to fetch object IDs for earthquakes that have a magnitude greater than 6.0
-	          // that occurred after January 1st 2007
-	          var query = new Query();
-	          query.where = "OBJECTID > 1";
-//	          query.timeExtent = new TimeExtent(new Date("01/01/2007 UTC"));
-	          featureLayer.queryIds(query, function (objectIds) {
-	        	  console.log("objectids:" + objectIds);
-	            fetchRecords(objectIds);
-	          });
-	          // Query for the records with the given object IDs and populate the grid
-//	            featureLayer.queryFeatures(query, function (featureSet) {
-//	            	console.log("featureSet:" + featureSet);
-//	              updateGrid(featureSet, pageNumber);
-//	            });
-	        });
 	        function fetchRecords(objectIds) {
 	            if (objectIds.length > 0) {
 	            	var query = new Query();
 	                query.objectIds = objectIds;
 	                query.outFields = ["*"];
 	                featureLayer.queryFeatures(query, function (featureSet) {
-	                	console.log("featureSet:" + featureSet);
-//	              		updateGrid(featureSet, pageNumber);
+	              		updateGrid(featureSet);
 	                });
-//	              updatePageInformation(objectIds);
-//	              queryRecordsByPage(1);
 	            } else {
 	              grid.showMessage("No matching records");
 	              grid.setStore(null);
 	            }
+	        };
+	        function updateGrid(featureSet) {
+//	        	grid.setStore(null);
+//	        	grid.fefresh();
+	        	var columns = {};
+	        	for (var i = 0; i < featureSet.fields.length; i++)
+	                columns[featureSet.fields[i].name] = featureSet.fields[i].name;
+    	        grid = new OnDemandGrid({
+  		          store: Memory({
+  		            idProperty: "OBJECTID"
+  		          }),
+  		          columns:columns,
+  		          sort: sortAttr
+  		        }, "grid");
+//    	        for (var i = 0; i < featureSet.fields.length; i++)
+//	                grid.columns[featureSet.fields[i].name] = featureSet.fields[i].name;
+	            var data = arrayUtils.map(featureSet.features, function (entry, i) {
+	              var data = {};
+	              for (var attribute in entry.attributes)
+	            	  data[attribute] = entry.attributes[attribute];
+	              return data;
+	            });
+	            grid.store.setData(data);
+	            grid.refresh();
 	        };
 	        function updatePageInformation(objectIds, page) {
 	            pageInfo = {
@@ -91,11 +79,16 @@
 	        	if (frist) {
 //	        		on(dom.byId("legend_operational0"),"click", function() { 
 	        		var length = $('#legend_operational0')[0].childNodes.length;
-	        		for (var i = 1; i < length; i++){
-	        		on($('#legend_operational0')[0].childNodes[i],"click", function(e) { 
-		            	if (isHide ) {
-		            		$("#webtable").animate({top:150,opacity:'show',width:500,height:300,left:'30%'},500);
+	        		var layerNumArr = [];
+	        		for (var n = 1; n < length; n++){
+	        		layerNumArr[$('#legend_operational0')[0].childNodes[n].innerText] = n;
+	        		on($('#legend_operational0')[0].childNodes[n],"click", function(e) { 	
+	        			if ($('#grid')) $('#grid').empty();
+	        			
+		            	if (isHide) {
+		            		$("#webtable").animate({top:150,opacity:'show',width:700,height:300,left:'17%'},500);
 		            		text = e.currentTarget.innerText;
+		            		
 		            		isHide = false;
 		            	}else if(text != e.currentTarget.innerText){
 		            		//
@@ -105,7 +98,25 @@
 		            		$("#webtable").animate({top:150,opacity: 'hide',width:0,height:0,left:0},500);
 		            		text = "";
 		            		isHide = true;
+		            		return;
 		            	}
+		            	
+		            	//dy add featureLayer
+	        			layerNum = layerNumArr[e.currentTarget.innerText];
+	        	        featureLayer = new FeatureLayer("http://192.168.2.127:6080/arcgis/rest/services/webgis/cx/MapServer/"+layerNum, {
+	        		          outFields:["*"]
+	        		        });
+	        	        featureLayer.on("load", function () {
+	        		          // create the query to fetch object IDs for earthquakes that have a magnitude greater than 6.0
+	        		          // that occurred after January 1st 2007
+	        		          var query = new Query();
+	        		          query.where = "OBJECTID > 0";
+//	        		          query.timeExtent = new TimeExtent(new Date("01/01/2007 UTC"));
+	        		          featureLayer.queryIds(query, function (objectIds) {
+//	        		        	  console.log("objectids:" + objectIds);
+	        		            fetchRecords(objectIds);
+	        		          });
+	        	        });
 		            });
 	        		frist = false;
 	        		}
