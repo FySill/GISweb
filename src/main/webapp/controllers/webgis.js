26f531753d3bc48c0047d8866a8af1d03ba30e47
chengfei <fysill@hotmail.com>
2014-10-06 00:04:36 +0800

    polygon

--- src/main/webapp/controllers/webgis.js
+++ src/main/webapp/controllers/webgis.js
@@ -1,15 +1,17 @@
 //function webGis($scope) {
 //	$scope.loadingData = function () {
 		var layerNum = 1;
-	    require(["dojo/on", "dojo/query", "dojo/parser", "esri/layers/FeatureLayer", "esri/tasks/query",
-	             "dojo/ready", "dijit/layout/BorderContainer", 
-	             "dijit/layout/ContentPane", "dojo/dom", "esri/map", 
+		var map;
+	    require(["dojo/on", "dojo/query", "dojo/parser", "esri/geometry/Polyline", "esri/layers/FeatureLayer", "esri/tasks/query",
+	             "dojo/ready", "dijit/layout/BorderContainer", "esri/layers/GraphicsLayer",
+	             "dijit/layout/ContentPane", "dojo/dom", "esri/map", "esri/graphic",
 	             "esri/geometry/Extent", "esri/urlUtils", "esri/arcgis/utils", 
 	             "esri/dijit/Legend", "esri/dijit/Scalebar","dojo/_base/array",
 	             "dgrid/OnDemandGrid", "dojo/store/Memory", "dojo/domReady!"], 
-        function( on, query, parser, FeatureLayer, Query, ready, BorderContainer, ContentPane, 
-        		dom, Map, Extent, urlUtils, arcgisUtils, Legend, Scalebar,arrayUtils,
+        function( on, query, parser, Polyline, FeatureLayer, Query, ready, BorderContainer, GraphicsLayer,
+        		ContentPane, dom, Map, Graphic, Extent, urlUtils, arcgisUtils, Legend, Scalebar,arrayUtils,
         		OnDemandGrid, Memory) {
+	    	console.log("webgis");
 	    	var featureLayer, pageInfo, grid;
 	    	// create a dgrid 
 	        var sortAttr = [{
@@ -30,8 +32,6 @@
 	            }
 	        };
 	        function updateGrid(featureSet) {
-//	        	grid.setStore(null);
-//	        	grid.fefresh();
 	        	var columns = {};
 	        	for (var i = 0; i < featureSet.fields.length; i++)
 	                columns[featureSet.fields[i].name] = featureSet.fields[i].name;
@@ -77,7 +77,6 @@
 	        	var isHide = true;
 	        	var text = "";
 	        	if (frist) {
-//	        		on(dom.byId("legend_operational0"),"click", function() { 
 	        		var length = $('#legend_operational0')[0].childNodes.length;
 	        		var layerNumArr = [];
 	        		for (var n = 1; n < length; n++){
@@ -124,9 +123,58 @@
 	        	
 //	          map.setBasemap("streets");
 	        });
-//	        on(dom.byId("btnSatellite"),"click", function() { 
-////	          map.setBasemap("satellite");
-//	        });
+	        on(dom.byId("btnImport"),"click", function() { 
+//				var length = $('#legend_operational0')[0].childNodes.length;
+//				var layerNumArr = [];
+//				for (var n = 1; n < length; n++){
+//				layerNumArr[$('#legend_operational0')[0].childNodes[n].innerText] = n;
+//				}
+//				layerNum = layerNumArr["边界"];
+		        featureLayer = new FeatureLayer("http://192.168.2.127:6080/arcgis/rest/services/webgis/cx/MapServer/6", {
+			          outFields:["*"]
+			    });
+    	        featureLayer.on("load", function () {
+  		          var query = new Query();
+  		          query.where = "OBJECTID > 0";
+  		          featureLayer.queryIds(query, function (objectIds) {
+  		            if (objectIds.length > 0) {
+  		            	var query = new Query();
+  		                query.objectIds = objectIds;
+  		                query.outFields = ["*"];
+  		                featureLayer.queryFeatures(query, function (featureSet) {
+  		                	extendPoint = [];
+  		                	extendPoint = featureSet.features[0].geometry.paths[0];
+//  		                	for(var point in featureSet.features[0].geometry.paths[0]){
+//  		                		extendPoint.push(featureSet.features[0].geometry.paths[0][point]);
+//  		                	}
+//  		                	var rings = [];
+////  		                	rings.push(extendPoint)
+//  		    		        var myPolygon = {"geometry":{"rings":extendPoint,
+//						                                "spatialReference":{latestWkid: 2384, wkid: 2384}
+//						     							},
+//						                    "symbol":{"color":[0,0,0,64],
+//						                   	 	   "outline":{"color":[0,0,0,255],
+//						                   	 		   		  "width":1,
+//						                   	 		   		  "type":"esriSLS","style":"esriSLSSolid"},
+//						                   	 	   "type":"esriSFS","style":"esriSFSSolid"}
+//						     				};
+//  		    		      var gl = new GraphicsLayer({id: "myPolygon"});
+//	  		  	          var gra = new Graphic(myPolygon);
+//	  		  	          map.addLayer(gl);
+//	  		  	          gl.add(gra);
+  		                	var polylineJson = {
+  		                		    "paths":extendPoint,
+  		                		    "spatialReference":{latestWkid: 2384, wkid: 2384}
+  		                		  };
+  		                	var gl = new GraphicsLayer({id: "myPolygon"});
+  		                		  var polyline = new Polyline(polylineJson);
+  		                		map.addLayer(gl);
+  		                		gl.add(polyline);
+  		                });
+  		            }
+  		       });  
+  	        });
+        });
 //	        on(dom.byId("btnHybrid"),"click", function() { 
 //	          map.setBasemap("hybrid");
 //	        });
@@ -179,7 +227,22 @@
 	          "version": "1.1"
 	        };
 	        arcgisUtils.createMap(webmap,"map").then(function(response){
-	          var map = response.map;
+	          map = response.map;
+	          
+//	          var myPolygon = {"geometry":{"rings":[[[60303,44034],[64461,44082],[64470,44959],[65209,45075],[60303,44034]]],
+//						                  "spatialReference":{latestWkid: 2384, wkid: 2384}
+//											},
+//						      "symbol":{"color":[2,0,0,64],
+//						     	 	   "outline":{"color":[0,0,0,255],
+//						     	 		   		  "width":1,
+//						     	 		   		  "type":"esriSLS","style":"esriSLSSolid"},
+//						     	 	   "type":"esriSFS","style":"esriSFSSolid"}
+//								};
+//	          var gl = new GraphicsLayer({id: "myPolygon"});
+//	          var gra = new Graphic(myPolygon);
+//	          map.addLayer(gl);
+//	          gl.add(gra);
+	          
 		        var legendLayers = arcgisUtils.getLegendLayers(response); 
 		          var legendDijit = new Legend({
 		            map: map,
